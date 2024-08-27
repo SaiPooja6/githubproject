4c16e406e6c7ac25bdc01829f787e9764a18d7d8
SaiPooja6 <poojitha.yarlagadda3230@gmail.com>
2024-08-27 16:29:55 -0500

    created a function for adding text to the grayscaled image.

--- src/githubproject/pipelines/newpipeline/nodes.py
+++ src/githubproject/pipelines/newpipeline/nodes.py
@@ -3,7 +3,7 @@ This is a boilerplate pipeline 'newpipeline'
 generated using Kedro 0.19.8
 """
 
-from PIL import Image
+from PIL import Image, ImageDraw
 
 #creating a function that converts the colored image to grayscale image.
 def grayscaled_image(image:Image.Image)->Image.Image:
@@ -11,6 +11,13 @@ def grayscaled_image(image:Image.Image)->Image.Image:
     return image
 def resized_image(image:Image.Image)->Image.Image:
     image = image.resize((300, 300))
+    return image
+def addtext(image:Image.Image)->Image.Image:
+    draw = ImageDraw.Draw(image)
+    text = "I am grayscaled image"
+    font_size = 20
+    position = (10,200)
+    draw.text(position, fill = "red", text=text )
     return image
 
 
