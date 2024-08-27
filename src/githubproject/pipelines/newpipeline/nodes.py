3f53c25de53d28e5393307948b091aed60c9a8a6
SaiPooja6 <poojitha.yarlagadda3230@gmail.com>
2024-08-27 14:16:11 -0500

    Added the pipeline

--- src/githubproject/pipelines/newpipeline/nodes.py
+++ src/githubproject/pipelines/newpipeline/nodes.py
@@ -6,7 +6,7 @@ generated using Kedro 0.19.8
 from PIL import Image
 
 #creating a function that converts the colored image to grayscale image.
-def grayscale_image(image:Image.image)->Image.Image:
+def grayscaled_image(image:Image.Image)->Image.Image:
     image = image.convert("L")
     return image
 
--- src/githubproject/pipelines/newpipeline/pipeline.py
+++ src/githubproject/pipelines/newpipeline/pipeline.py
@@ -3,8 +3,14 @@ This is a boilerplate pipeline 'newpipeline'
 generated using Kedro 0.19.8
 """
 
-from kedro.pipeline import Pipeline, pipeline
-
-
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import grayscaled_image
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func = grayscaled_image,
+            inputs = "colored_image",
+            outputs = "grayscaled_image1",
+            name = "grayscaled_image",
+        )
+    ])
